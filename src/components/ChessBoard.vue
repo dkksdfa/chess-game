f9e8d41e6536bf0d9cbe3e07b1cc440cccae4f4f
dkksdfa <tjdalstjdals100@gmail.com>
2022-05-12 08:01:15 +0900

    piece img add

--- src/components/ChessBoard.vue
+++ src/components/ChessBoard.vue
@@ -2,17 +2,18 @@
   <div class="board">
     <div v-for="rank in boardList" :key="rank.id" class="rank">
       <div v-for="file in rank" :key="file.id" class="file">
-        {{file}}
+        <ChessPiece :dataInt="file" v-if="file != 0"></ChessPiece>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-
+import ChessPiece from './ChessPiece.vue'
 export default {
   name: 'ChessBoard',
   components: {
+    ChessPiece
   },
   props: {
     boardList: Array
@@ -28,5 +29,8 @@ export default {
 <style scoped>
   .file {
     display: inline-block;
+    width: 50px;
+    height: 50px;
+    border: 1px solid black;
   }
 </style>
---
+++ src/components/ChessPiece.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="piece">
+    <img :src="require(`../assets/${dataInt}.png`)"
+    draggable=true @dragstart="dragStart" @click="ab">
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'ChessPiece',
+  props: {
+    dataInt: Number
+  },
+  methods: {
+    ab () {
+      console.log('클릭')
+    },
+    dragStart () {
+      console.log('드래그')
+    }
+  }
+}
+</script>
+
+<style scoped>
+img {
+  width: 50px;
+  cursor: move;
+}
+</style>
